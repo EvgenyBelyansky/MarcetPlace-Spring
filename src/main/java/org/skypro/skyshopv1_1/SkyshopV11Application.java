bd8fb84bb9c7fab03f419481f35b1dcd66318b58
anzam <e.g.belyansky@gmail.com>
2025-06-30 19:49:32 +0400

    initial commit

---
+++ src/main/java/org/skypro/skyshopv1_1/SkyshopV11Application.java
@@ -0,0 +1,13 @@
+package org.skypro.skyshopv1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyshopV11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyshopV11Application.class, args);
+	}
+
+}
